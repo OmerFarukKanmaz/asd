42d0f0cd0414c55010c24e9340ce159bbf06823e
OmerFarukKanmaz <kanmazomerfaruk@otulook.com>
2023-07-21 13:05:37 +0300

    state up

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -2,16 +2,42 @@ import { useState } from 'react'
 import { PropTypes } from 'prop-types';
 import './Counter.css'
 
-export default function Counter({ by }) {
+export default function Counter() {
 
     const [count, setCount] = useState(0);
 
+    function increment(by) {
+        setCount(count + by)
+    }
+
+    function decrement(by) {
+        setCount(count - by)
+
+    }
+
+    return (
+        <>
+            <span className="totalCount">{count}</span>
+            <CounterButton by={1} increment={increment} decrement={decrement} />
+            <CounterButton by={2} increment={increment} decrement={decrement} />
+            <CounterButton by={3} increment={increment} decrement={decrement} />
+        </>
+    )
+}
+
+function CounterButton({ by, increment, decrement }) {
+
+    const [count, setCount] = useState(0)
+
     function incrementCounterFunction() {
         setCount(count + by)
+        increment(by)
     }
     function decrementCounterFunction() {
         setCount(count - by)
+        decrement(by)
     }
+
     return (
         <div className="Counter">
             <span className="count">{count}</span>
@@ -27,11 +53,10 @@ export default function Counter({ by }) {
         </div>
     )
 }
-
-Counter.protoType = {
+CounterButton.protoType = {
     by: PropTypes.number
 }
 
-Counter.defaultProps = {
+CounterButton.defaultProps = {
     by: 5
-}
+};
